2b58377101f4ae08dc6f0d2805606222f5ed0d90
Vincent Philaeux Lamotte <philaeux@the-cluster.org>
2018-07-16 21:48:37 +0200

    New design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule, MatIconModule, MatButtonModule } from "@angular/material";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule} from "@angular/material";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { IndexComponent } from './index/index.component';
-import { ObsControlComponent } from './obs-control/obs-control.component';
-import { VodManageComponent } from './vod-manage/vod-manage.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { UserControlComponent } from './user-control/user-control.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {IndexComponent} from './index/index.component';
+import {ObsControlComponent} from './obs-control/obs-control.component';
+import {VodManageComponent} from './vod-manage/vod-manage.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {UserControlComponent} from './user-control/user-control.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,7 @@ import { UserControlComponent } from './user-control/user-control.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatMenuModule, MatIconModule, MatButtonModule
+    MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
