6fe20d3c7c4be35b3df7fd37366da738bde4be5d
kikisaeba <kikisaeba@gmail.com>
2018-11-25 12:01:51 +0100

    [Attention] Regexp should be review carefully

--- src/app/shared/csv/csv_helpers.ts
+++ src/app/shared/csv/csv_helpers.ts
@@ -1,29 +1,30 @@
 export default class CsvHelper {
 
   static CSVtoArray(csv) {
-    let objPattern = new RegExp((
+    const objPattern = new RegExp((
       // Delimiters.
-      "(\\" + "," + "|\\r?\\n|\\r|^)" +
+      '(\\' + ',' + '|\\r?\\n|\\r|^)' +
       // Quoted fields.
-      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+      '(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|' +
       // Standard fields.
-      "([^\"\\" + "," + "\\r\\n]*))"), "gi");
-    let arrData = [[]];
-    var arrMatches = null;
+      '([^\"\\' + ',' + '\\r\\n]*))'), 'gi');
+    const arrData = [[]];
+    let arrMatches = null;
     while (arrMatches = objPattern.exec(csv)) {
-      var strMatchedDelimiter = arrMatches[1];
-      if (strMatchedDelimiter.length && (strMatchedDelimiter != ",")) {
+      const strMatchedDelimiter = arrMatches[1];
+      if (strMatchedDelimiter.length && (strMatchedDelimiter !== ',')) {
         arrData.push([]);
       }
+      let strMatchedValue;
       if (arrMatches[2]) {
-        let strMatchedValue = arrMatches[2].replace(
-          new RegExp("\"\"", "g"), "\"");
+        strMatchedValue = arrMatches[2].replace(
+          new RegExp('\"\"', 'g'), '\"');
       } else {
-        var strMatchedValue = arrMatches[3];
+        strMatchedValue = arrMatches[3];
       }
       arrData[arrData.length - 1].push(strMatchedValue);
     }
-    if (arrData[arrData.length - 1].length == 0) {
+    if (arrData[arrData.length - 1].length === 0) {
       arrData.splice(arrData.length - 1, 1);
     }
     return arrData;
